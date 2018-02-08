8316f9b3806852d66415346313e5010d2be039be
crearthur <le-tutur@hotmail.fr>
2018-02-08 17:21:58 +0100

    Upgrade Games collections into acordions and some fixs

--- js/init.js
+++ js/init.js
@@ -8,7 +8,9 @@
       hoverEnabled: true, // Hover enabled
       toolbarEnabled: false // Toolbar transition enabled
     });
-    $('input#icon_prefix, textarea#textarea').characterCounter();    
+    $('input#icon_prefix, textarea#textarea').characterCounter();
+    $('.tooltipped').tooltip({ delay: 50 }); // enable tooltips
+    $('.collapsible').collapsible(); // enable collapsible
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
