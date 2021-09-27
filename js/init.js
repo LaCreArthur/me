cf542ed0ce1626392703c8720cce05c7497a061f
Arthur <le-tutur@hotmail.fr>
2021-09-27 17:23:28 +0200

    added mobile game, updated images

--- js/init.js
+++ js/init.js
@@ -12,5 +12,18 @@
     $('.tooltipped').tooltip({ delay: 50 }); // enable tooltips
     $('.collapsible').collapsible(); // enable collapsible
 
+    // $('#target-div').load('https://play.google.com/store/apps/developer?id=Bretzel+Studio .N4FjMb', function( response, status, xhr ) {
+    //   if ( status == "error" ) {
+    //     var msg = "Sorry but there was an error: ";
+    //     $( "#target-div" ).html( msg + xhr.status + " " + xhr.statusText );
+    //   }
+    // });
+
+    var iframe   = $('#inlineFrameExample').contents(),
+    loginBox = iframe.find(".VjFXz").clone();
+
+    iframe.find("*").remove();
+    iframe.append(loginBox);
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
